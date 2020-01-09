bebc5756a69c8ff1a35fcf5f5371091958f0c17d
Zachary Spielberger <zachspielberger@gmail.com>
2020-01-09 14:17:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,43 @@
 from flask import Flask, jsonify, render_template, request, Response, redirect, url_for
 import json
 from scraping import getInfo
+# from scraping import topBoxOffice
 
 app = Flask(__name__)
 
 
 @app.route('/', methods = ["POST", "GET"])
 def index():
-	if request.method == "POST":
+
+	# info = topBoxOffice()
+
+	# Render blank template
+	#return render_template('index.html',
+		#boxOffice = info,clearButton="none")
+	return render_template('index.html')
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+@app.route('/search', methods = ["POST","GET"])
+def search():
+	if request.method == 'POST':
 		posts = request.form
 		movie_name = posts['movie'].title()
 
-		info = getInfo(movie_name)
+		theatre = request.form.getlist('check')
+		print(theatre)
+		
+		if(len(theatre) == 0):
+			return render_template('search.html',clearButton="none")
+
+		info = getInfo(movie_name,theatre)
 		
 		return render_template(
-			'index.html',
+			'search.html',
 			movies = info,
-			clearButton = 'visible'
+			clearButton = 'visible',
+			className = 'py-5 bg-light',
+			
 		)
 
-	# Render blank template
-	return render_template('index.html',clearButton="none")
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+	return render_template('search.html',clearButton="none")
